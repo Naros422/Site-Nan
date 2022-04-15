daa9bdad8719aa53b5d0e026124886bd803b8185
Naros422 <nurkenn82@gmail.com>
2022-04-15 17:36:50 +0600

    maint commit

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Header.css'
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
